c933719bfc023fc9e08824b4086bd7b8a0b79260
Demetrusa <demetre.kurdadze20b@geolab.edu.gegit config --global user.name Demetrusacd /Desktop/Users/Git/Demegit config --global user.email demetre.kurdadze20b@geolab.edu.ge>
2021-11-21 19:00:01 +0400

    js examples array

---
+++ scritp.js
@@ -0,0 +1,47 @@
+// // array filter & include examples 
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+//     const result = words.filter(word => word.length > 7);
+//         console.log(result);
+
+// const patches = ['classic', 'tbc', 'wotlk', 'cataclysm', 'pandarea', 'draenor','legion', 'bfa','shadowlands'];
+//     const patch = patches.filter(word => word.length < 6);
+//     const some = patches.filter(word => word.includes("o"))
+//         console.log(some);
+//         console.log(patch);
+
+    const array1 = [2254, 5236, 7623, 123931, 25026];
+        const something = array1.filter(num => num.includes(2));
+        console.log(something)
+        // aq ver gamovitane ragac ar gamoaq. mindoda rom xelit gamometana is cifri romelic sheicavda 2 s mara anonymous miwers
+
+// let wordi = ['classic', 'tbc', 'wotlk', 'legion', 'pandarea'];
+//     const modifedWords = wordi.filter((word, index, arr) => {
+//         arr[index+1] += 'extra'
+//         return word.lenght <6
+//     }
+//     )
+//     console.log(modifedWords);
+    
+// words = ['classic', 'tbc', 'wotlk', 'legion', 'pandarea'];
+//     const deleteWords = words.filter( (word, index, arr) => {
+//         arr.pop()
+//         return word.length <= 7
+// })
+
+// console.log(deleteWords);
+
+// number example
+
+// const array = [-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7];
+//     function line(num){
+//         for( let i=2; num >i; i++){
+//             if(num % i == 0){
+//                 return false;
+//             }
+//         }
+//         return num > 1;
+//     }
+//     console.log(array.filter(line));
+
+// aq ver mivxvdi ratom amoigo 2 cifri, rom gaiyofa 2ze nashti ar rcheba da wesit ar unda amoego
+
